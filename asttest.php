337035b48917c67e8d961e353c1f04a57d3f9ea1
GoRuGoo <dachanyr8989@gmail.com>
2024-04-11 14:22:37 +0900

    feat: トラバースして人間が見える文字で表示するところまでは実装した

--- asttest.php
+++ asttest.php
@@ -3,17 +3,18 @@ require __DIR__ . '/vendor/autoload.php';
 use PhpParser\Error;
 use PhpParser\NodeDumper;
 use PhpParser\ParserFactory;
-
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Function_;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitorAbstract;
+use PhpParser\PrettyPrinter;
 $code = <<<'CODE'
+
 <?php
 
 function test($foo)
 {
     var_dump($foo);
-}
-
-function testhogehoge($hoge){
-test($hoge);
 }
 
 CODE;
@@ -26,6 +27,26 @@ try {
     echo "Parse error: {$error->getMessage()}\n";
     return;
 }
+
+//トラバース(変更・加工)する
+$traverser = new NodeTraverser();
+$traverser->addVisitor(new class extends NodeVisitorAbstract {
+    public function enterNode(Node $node) {
+        if ($node instanceof Function_) {
+            // Clean out the function body
+            $node->stmts = [];
+        }
+    }
+});
+$ast = $traverser->traverse($ast);
+
+$prettyPrinter = new PrettyPrinter\Standard;
+echo $prettyPrinter->prettyPrintFile($ast);
+
+
+echo "\n";
+echo "-----------------------------------------------------";
+echo "\n";
 
 $dumper = new NodeDumper;
 echo $dumper->dump($ast) . "\n";
